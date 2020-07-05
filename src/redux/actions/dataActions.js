4ea3a1b457ebd07402440f27c6912cafb17074e6
lenn0n <gameoveralisa@gmail.com>
2020-07-05 22:26:05 +0800

    Added Cross-Origin

--- src/redux/actions/dataActions.js
+++ src/redux/actions/dataActions.js
@@ -4,7 +4,7 @@ import { SET_SCREAMS } from "../types";
 import axios from "axios";
 export const getAllScreams = () => (dispatch) => {
   axios
-    .get("/screams")
+    .get("https://asia-east2-fir-crud-8d71b.cloudfunctions.net/api/screams")
     .then((res) => {
       dispatch({ type: SET_SCREAMS, payload: res.data });
     })
