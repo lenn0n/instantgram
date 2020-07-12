c71476120f6d94226b282b798324a08674a7271c
lenn0n <gameoveralisa@gmail.com>
2020-07-12 09:42:15 +0800

    Enchanced Auth Module

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,21 +1,27 @@
 import React from "react";
 import jwtDecode from "jwt-decode";
 import { Redirect } from "react-router-dom";
+
 export const authModule = (isSecure) => {
+  //get the token from local storage
+  const token = localStorage.FBIdToken;
   if (isSecure) {
-    const token = localStorage.FBIdToken;
     if (token) {
+      //decode it using jwt
       const decodedToken = jwtDecode(token);
+      //check if token is expired
       if (decodedToken.exp * 1000 < Date.now()) {
         return <Redirect to="/login" />;
       }
     } else {
+      //no token found
       return <Redirect to="/login" />;
     }
   } else if (!isSecure) {
-    const token = localStorage.FBIdToken;
     if (token) {
+      //decode it using jwt
       const decodedToken = jwtDecode(token);
+      //check if token is valid
       if (decodedToken.exp * 1000 > Date.now()) {
         return <Redirect to="/" />;
       }
